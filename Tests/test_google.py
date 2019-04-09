e19a29426a7da45ed5e3de6e8ac7aab16e3a0a3c
Anatolii Kasianov <tommy@macpaw.com>
2019-04-09 21:47:54 +0300

    Add run script and export variables

---
+++ Tests/test_google.py
@@ -0,0 +1,52 @@
+import os
+import time
+import pytest
+from selenium import webdriver
+browser = os.getenv('BROWSER')
+environment = os.getenv('ENVIRONMENT')
+
+
+class TestGooglePage():
+
+    @classmethod
+    def setup_class(cls):
+        "Runs once per class"
+
+    @classmethod
+    def teardown_class(cls):
+        "Runs at end of class"
+        # driver.quit()
+
+    def setup(self):
+        global driver
+        if "local" in environment:
+            if "chrome" in browser:
+                driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+            if "firefox" in browser:
+                driver = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
+        else:
+            driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities={"browserName": browser})
+
+        driver.get("https://www.google.com.ua/")
+        
+
+    def teardown(self):
+        # driver.close()
+        driver.quit()
+
+    # Tests
+    def test_logo_is_present(self):
+        google_logo = driver.find_element_by_xpath("//*[@id=\"hplogo\"]")
+        assert self.is_element_exists(google_logo)
+
+    def test_Pass(self):
+        google_logo = driver.find_element_by_xpath("//*[@id=\"hplogo\"]")
+        assert self.is_element_exists(google_logo)
+
+    # Helper function
+    def is_element_exists(self, element):
+        try:
+            if element is not None:
+                return True
+        except NoSuchElementException:
+            return False
---
+++ run-tests.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+function help() {
+    echo "Usage: $(basename "$0") [-b|--browser] <browser_name> [-p|--path] <path/to/tests> [-t|--test] <test_name> [-e|--environment] <local/ci>"
+    echo ""
+    echo "Options:"
+    echo "  -b, --browser 		set browser name"
+    echo "  -p, --path 			path to tests directory or directly to test file"
+    echo "  -t, --test 			name of test or name pattern"
+    echo "  -e, --environment		set environment local or CI (key is not necessary if CI)"
+    echo "  -h, --help 			display this help"
+    echo ""
+    echo "Examples:"
+    echo "  $(basename "$0") -b chrome -p Tests/ -t test_login -e local"
+    echo "  $(basename "$0") -b firefox -p Tests/"
+    exit 0
+}
+
+while test $# -gt 0; do
+    case $1 in
+    	-b|--browser)
+    		shift
+            if test $# -gt 0; then
+         		BROWSER_NAME="$1"
+         	fi
+            ;;
+	    -p|--path) 
+			shift
+            if test $# -gt 0; then
+            	TESTS_DIR_OR_FILE="$1"
+            fi
+	        ;;
+        -t|--test) 
+			shift
+            if test $# -gt 0; then
+				TEST_TO_RUN="-k $1"
+			fi
+            ;;
+        -e|--environment) 
+			shift
+            if test $# -gt 0; then
+				ENV="$1"
+			fi
+            ;;
+	    -h|--help)
+            help;;
+	    *) echo "Unknown option $1. Run with --help or -h for help."
+            exit 1;;
+    esac
+    shift
+done
+
+export BROWSER="$BROWSER_NAME"
+export ENVIRONMENT="$ENV"
+
+pytest -v "$TESTS_DIR_OR_FILE" "$TEST_TO_RUN"
+
+exit 0
